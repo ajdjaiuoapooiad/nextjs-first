7aa4644c44aeff9330cf785b9fa334e94e2d87c8
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2025-01-24 23:10:02 +0900

    ok

---
+++ src/pages/products/index.js
@@ -0,0 +1,14 @@
+import styles from '../../styles/Home.module.css'
+
+
+const ProductsList = () => {
+    return (
+        <div className={styles.contaienr}>
+            <main className={styles.main}>
+                <h2 className={styles.title}>商品一覧</h2>
+            </main>
+        </div>
+    );
+}
+
+export default ProductsList;

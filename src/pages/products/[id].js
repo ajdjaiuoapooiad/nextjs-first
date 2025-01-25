7157adbd75b93e3f78e27a733299a70b3a313136
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2025-01-25 13:27:01 +0900

    ok

--- src/pages/products/[id].js
+++ src/pages/products/[id].js
@@ -1,6 +1,39 @@
 import { useRouter } from "next/router";
+import Link from 'next/link';
+import styles from '../../styles/Home.module.css'
 
-const Product = () => {
+
+
+// SSGの場合
+export async function getStaticProps({ params }) {
+  //jsonファイルのデータを取りに行く。
+  const req = await fetch(`http://localhost:3000/${params.id}.json`);
+  const data = await req.json();
+
+  return {
+    props: { product: data },
+  };
+}
+
+export async function getStaticPaths() {
+  const req = await fetch("http://localhost:3000/products.json");
+  const data = await req.json();
+
+  const paths = data.map((product) => {
+    return {
+      params: {
+        id: product,
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+const Product = ({product}) => {
     const router = useRouter()
     console.log(router.query);
     const {id} = router.query
@@ -8,8 +41,18 @@ const Product = () => {
 
 
     return (
-        <div>
-            {id}
+        <div className={styles.contaienr}>
+            <main className={styles.main}>
+         
+                <h1>{id}のページです</h1>
+                <img src={product.image} width='300' height='400' />
+                <p>{product.name}</p>
+                <br />
+
+                <Link href='/products'>
+                    商品一覧へ
+                </Link>
+            </main>
         </div>
     );
 }
